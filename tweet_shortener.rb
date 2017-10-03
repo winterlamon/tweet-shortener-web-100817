c5f79e2eab4796beeef68d8064bfd2da17455b33
Winter LaMon <winter.lamon@gmail.com>
2017-10-03 12:35:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,8 +17,8 @@ end
 def word_substituter(tweet)
   tweet_array = tweet.split(" ")
   tweet_array.each do |word|
-    if tweet_array.include?(key)
-        dictionary.keys 
+    if tweet_array.include?(word) == dictionary.keys 
+      tweet_array.replace(dictionary.values)
     end
   end
 end
