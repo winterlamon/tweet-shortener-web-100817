447f48143f74fd1abdc594aeec3c492a5d042a35
Winter LaMon <winter.lamon@gmail.com>
2017-10-03 12:31:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,40 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  
+  tweet_array.each do |word|
+    if tweet_array.include?(key)
+        dictionary.keys = 
+    end
+  end
+end
+
+
+def bulk_tweet_shortener(array_of_tweets)
+
+end
+
+
+def sleective_tweet_shortener(tweet)
+
+end
+
+
+def shortened_tweet_truncator(tweet)
+
+end
