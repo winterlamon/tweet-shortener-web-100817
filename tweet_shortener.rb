49f54084f1a9cbd567e929f656793e49d01c602d
Winter LaMon <winter.lamon@gmail.com>
2017-10-03 13:42:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -49,7 +49,7 @@ end
 def shortened_tweet_truncator(tweet)
   selective_tweet_shortener(tweet)
   if tweet.length > 140
-    tweet[0..137] + "..."
+    tweet[0...137] + "..."
   else
     tweet
   end
